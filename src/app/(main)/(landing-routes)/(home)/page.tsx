afe5927ebba5b7093811aae55fc231282e02ba84
emmyd <emmanueldenis560@gmail.com>
2026-05-15 11:06:41 +0100

    feat: implemented three components

---
+++ src/app/(main)/(landing-routes)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Solution from "@/components/home/Solution";
+import Testimonial from "@/components/home/Testimonial";
+import FaqSection from "@/components/home/Faq";
+
+export default function Home() {
+  return (
+    <div>
+      <Solution />
+      <Testimonial />
+      <FaqSection />
+    </div>
+  );
+}
