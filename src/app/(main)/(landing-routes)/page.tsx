6e7134eca54d5eab2ae312a557aaf54cf586d0e1
ofe <ofeoritseamiteye03@gmail.com>
2026-05-15 10:11:09 +0100

    feat: app routing

---
+++ src/app/(main)/(landing-routes)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <>
+      <h1>import and add home page components here</h1>
+      {/* <HomeHero /> */}
+      {/* <SocialProof /> */}
+      {/* <WelcomeSection /> */}
+      {/* <Features /> */}
+      {/* <Testimonials /> */}
+      {/* <FaqSection /> */}
+      {/* <Create /> */}
+    </>
+  );
+}
