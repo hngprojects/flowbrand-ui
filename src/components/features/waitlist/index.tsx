338d29f4c1eb9e81db2baf8820cd936e40045607
unknown <aniduyakubu@yahoo.com>
2026-05-15 09:31:16 +0100

    feat: build waitlist page

--- src/components/features/waitlist/index.tsx
+++ src/components/features/waitlist/index.tsx
@@ -1,23 +1,64 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import WaitlistHeader from "./nav";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { joinWaitlist } from "@/services/waitlist.service";
+import { waitlistSchema } from "@/schema/waitlist.schema";
+import { WaitingUsers } from "@/components/icons/waiting-users";
 
 export default function WaitlistBody() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setError("");
+    setSuccess(false);
+    setLoading(true);
+
+    const result = waitlistSchema.safeParse({ email });
+
+    if (!result.success) {
+      setError("Enter a valid email");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      await joinWaitlist(email);
+
+      setSuccess(true);
+      setEmail("");
+    } catch (err) {
+      const errorMessage =
+        err instanceof Error ? err.message : "Failed to join waitlist";
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="mx-auto w-full max-w-[1440px] px-4 sm:px-6 lg:px-18 min-h-screen">
+    <div className="mx-auto w-full max-w-[1440px] min-h-screen px-4 sm:px-6 lg:px-18">
       <WaitlistHeader />
 
-      <div className="flex justify-between items-center mx-auto pt-4">
-        <div className="max-w-[480px]">
+      <div className="flex flex-col-reverse lg:flex-row justify-between items-center gap-10 lg:gap-6 pt-4 lg:pt-12">
+        <div className="w-full lg:max-w-[480px]">
           <div className="flex items-center gap-2 bg-primary-foreground rounded-full px-3 py-1.5 w-fit">
             <div className="bg-primary w-2 h-2 rounded-full"></div>
             <p className="text-primary text-xs font-semibold tracking-wide">
               AVAILABLE SOON
             </p>
           </div>
+
           <div>
-            <h1 className="text-5xl font-base leading-tight mt-6 mb-4 text-foreground">
+            <h1 className="text-4xl lg:text-5xl font-bold leading-tight mt-6 mb-4 text-foreground">
               Turn More Leads Into Paying{" "}
               <span className="text-primary">Customers</span>
             </h1>
@@ -28,43 +69,50 @@ export default function WaitlistBody() {
             </p>
           </div>
 
-          <form className="flex w-full max-w-[480px] gap-2">
-            <Input
-              type="email"
-              placeholder="you@gmail.com"
-              className="flex-1 h-12 px-4 rounded-xl bg-background border border-gray-800"
-            />
-            <Button type="submit" className="h-12 px-6 rounded-xl">
-              Join Waitlist
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col lg:flex-row w-full max-w-[480px] gap-3 lg:gap-2"
+          >
+            <div className="flex-1">
+              <Input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="you@gmail.com"
+                className={`flex-1 min-h-12 px-4 py-2 rounded-xl bg-background border ${
+                  error ? "border-red-500" : "border-gray-800"
+                }`}
+              />
+
+              {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
+              {success && (
+                <p className="text-green-600 text-sm mt-2">
+                  Congratulations! You are added to our waitlist
+                </p>
+              )}
+            </div>
+
+            <Button type="submit" disabled={loading} className="h-12">
+              {loading ? "Joining..." : "Join Waitlist"}
             </Button>
           </form>
 
-          <div className="flex items-center gap-3 mt-6">
-            <div className="flex -space-x-2">
-              <div className="w-8 h-8 rounded-full bg-red-600 border-2 border-background flex items-center justify-center text-xs font-medium text-primary-foreground">
-                GD
-              </div>
-              <div className="w-8 h-8 rounded-full bg-cyan-500 border-2 border-background flex items-center justify-center text-xs font-medium">
-                TY
-              </div>
-              <div className="w-8 h-8 rounded-full bg-orange-500 border-2 border-background flex items-center justify-center text-xs font-medium">
-                MU
-              </div>
-              <div className="w-8 h-8 rounded-full bg-green-700 border-2 border-background flex items-center justify-center text-xs font-medium">
-                MU
-              </div>
-            </div>
+          <div className="flex flex-col lg:flex-row items-center gap-3 mt-6">
+            <WaitingUsers />
             <p className="text-sm text-foreground">
               20+ businesses already waiting
             </p>
           </div>
         </div>
-        <div>
+
+        <div className="w-full lg:w-auto flex justify-center">
           <Image
             src="/images/laptop-screen.png"
-            width={600}
-            height={600}
+            width={700}
+            height={700}
             alt="laptop screen"
+            className="max-w-auto min-h-auto"
+            loading="eager"
           />
         </div>
       </div>
--- src/views/WaitlistView.tsx
+++ src/views/WaitlistView.tsx
@@ -1,12 +1,20 @@
 import WaitlistBody from "@/components/features/waitlist";
-import { PatternWaitlist, WaitlistBottom } from "@/components/icons";
+import {} from "@/components/icons";
+import { PatternWaitlist } from "@/components/icons/pattern-svg";
+import {
+  WaitlistBottom,
+  WaitlistBottomSmall,
+} from "@/components/icons/waitlist-bottom";
 
 export default function WaitlistView() {
   return (
     <div className="relative bg-gradient-to-r from-primary-100/20 via-primary-100/40 to-primary-100/60">
       <PatternWaitlist className="absolute inset-0 h-full w-full object-cover opacity-40 pointer-events-none -z-10" />
       <WaitlistBody />
-      <WaitlistBottom className="mx-auto w-full" />
+      <div className="overflow-hidden w-full">
+        <WaitlistBottom className="lg:block md:block hidden" />
+        <WaitlistBottomSmall className="lg:hidden md:hidden flex" />
+      </div>
     </div>
   );
 }
