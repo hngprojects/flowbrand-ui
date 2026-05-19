c509b5d06ab7753276d293838db7126c0e5dbfa4
Onuoha Chiemerie <onuohaemma167@gmail.com>
2026-05-19 09:16:42 +0100

    add Google OAuth authentication with NextAuth

---
+++ lib/auth.ts
@@ -0,0 +1,29 @@
+import GoogleProvider from "next-auth/providers/google";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        // @ts-ignore
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+};
